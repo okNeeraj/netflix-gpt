c85bd19d8c8911c762abfcc4a2e1cf157480f16d
Neeraj Singh <neerajonetwo@gmail.com>
2023-09-21 10:07:57 +0530

    Phase 02 - Install/Configure firebase and netflixGPT deployed on firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCpfXLqAelC5HYfyrt6jYfQR5MkaYU2k9w",
+  authDomain: "netflixgpt-aaf25.firebaseapp.com",
+  projectId: "netflixgpt-aaf25",
+  storageBucket: "netflixgpt-aaf25.appspot.com",
+  messagingSenderId: "851568533147",
+  appId: "1:851568533147:web:c1915f02b0ce93ffaf303e",
+  measurementId: "G-CT2V2NGGPV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
