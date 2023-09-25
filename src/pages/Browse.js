c52a31ce7857a945db9ac4bf3c7dffa4bf23d013
Neeraj Singh <neerajonetwo@gmail.com>
2023-09-25 18:03:33 +0530

    Phase 04 - Now playing movie card created

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,23 +1,12 @@
-import { useSelector } from 'react-redux';
-import useMovie from '../hooks/useMovie'
-import { MOVIES } from '../services/tmdb';
 import Showcase from '../components/Showcase';
 import MovieSlider from '../components/MovieSlider';
 
 const Browse = () => {
-  // const { nowPlaying } = MOVIES;
-
-  // Fetch movies and set to Redux Store
-  // useMovie(nowPlaying.endpoint, nowPlaying.type);
-
-  // Receive moview list from Redux Store
-  // const nowPlayingList = useSelector((store) => store.movies.nowPlaying);
-  // if (nowPlayingList === null) return <h1>Not found</h1>;
-
   return (
     <div className='broswe-page'>
       <Showcase />
-      <div className='moview-by-type'>
+      <div className='moview-by-type px-4 md:px-12 mt-[-130px] z-50 relative'>
+        <h4 className='mb-3 text-[20px] text-[#e5e5e5]'>Now Playing</h4>
         <MovieSlider />
       </div>
     </div>
--- src/services/tmdb.js
+++ src/services/tmdb.js
@@ -6,6 +6,8 @@ export const TMDB_OPTIONS = {
   }
 };
 
+export const TMDB_CDN_URL = "https://image.tmdb.org/t/p/"
+
 export const TMDB_API_URL = "https://api.themoviedb.org/3";
 
 export const MOVIES = {
