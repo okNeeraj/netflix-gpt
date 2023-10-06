24d9c243867d39a77999ae0686ab528c5c22360b
Neeraj Singh <neerajonetwo@gmail.com>
2023-10-06 08:20:59 +0530

    Phase 06 - Slider functionality implemented for movie slider

--- src/components/MovieSlider.js
+++ src/components/MovieSlider.js
@@ -1,17 +1,47 @@
+import { useState } from 'react';
+import SwiperCore from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 import MovieCard from "./MovieCard"
 
+SwiperCore.use([Navigation, Pagination]);
+
 const MovieSlider = ({ heading, data }) => {
+  const [slidesPerGroup, setSlidesPerGroup] = useState(2);
+  const handleResize = () => {
+    const slidesInView = Math.floor(window.innerWidth / 144);  // Adjust 200 to your slide width
+    setSlidesPerGroup(slidesInView);
+  };
+
   if (!data) return;
   const movies = data.results;
 
   return (
     <div className="movie-slider mb-8">
       <h4 className='mb-3 text-[20px] text-[#e5e5e5]'>{heading}</h4>
-      <div className="flex gap-4 overflow-auto">
+      <Swiper
+        slidesPerView={'auto'}
+        spaceBetween={15}
+        slidesPerGroup={1}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Navigation]}
+        onResize={handleResize}
+        className="mySwiper flex overflow-visibleF"
+      >
         {
-          movies.map((movie) => <MovieCard key={movie.id} data={movie} />)
+          movies.map((movie) => (
+            <SwiperSlide key={movie.id} className='cursor-pointer flex-grow-0 flex-shrink-0 w-28 md:w-36 overflow-hidden rounded'>
+              <MovieCard data={movie} />
+            </SwiperSlide>)
+          )
         }
-      </div>
+      </Swiper>
     </div>
   )
 }
--- src/components/TrendingSlider.js
+++ src/components/TrendingSlider.js
@@ -1,17 +1,37 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 import TrendingCard from "./TrendingCard"
+import { Navigation } from 'swiper/modules';
+import { useState } from 'react';
 
 const TrendingSlider = ({ heading, data }) => {
   if (!data) return;
   const movies = data.results;
 
   return (
-    <div className="movie-slider mb-8">
+    <div className='movie-slider mb-8'>
       <h4 className='mb-3 text-[20px] text-[#e5e5e5]'>{heading}</h4>
-      <div className="flex gap-4 overflow-auto">
+      <Swiper
+        slidesPerView={'auto'}
+        spaceBetween={15}
+        slidesPerGroup={1}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper flex"
+      >
         {
-          movies.map((movie, index) => <TrendingCard key={movie.id} index={index + 1} data={movie} />)
+          movies.map((movie, index) => (
+            <SwiperSlide key={movie.id} className='cursor-pointer flex-grow-0 flex-shrink-0 w-52 overflow-hidden rounded'>
+              <TrendingCard key={movie.id} index={index + 1} data={movie} />
+            </SwiperSlide>)
+          )
         }
-      </div>
+      </Swiper>
     </div>
   )
 }
