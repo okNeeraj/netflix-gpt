ada2f95c35bae2c93ecf051375c9f4f1c561146b
Neeraj Singh <neerajonetwo@gmail.com>
2023-10-10 07:31:05 +0530

    Phase 07 - Logo updated and index layer issue fixed on search page

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const BASE_URL = process.env.REACT_APP_BASE_URL;
 export const LOGO_ICON = `${BASE_URL}/netflix-gpt.png`;
-export const LOGO_RED = `${BASE_URL}/ngpt-red-300x71.png`;
+export const LOGO_RED = `${BASE_URL}/ngpt-red-300x71-01.png`;
 export const LOGO_WHITE = `${BASE_URL}/ngpt-white-300x71.png`;
 export const POSTER_BG = `${BASE_URL}/poster-bg.jpeg`;
 export const BACKDROP = `${BASE_URL}/blur.png`;
