6786992d96cef1532727fe3298e4d4c4e26d158c
Neeraj Singh <neerajonetwo@gmail.com>
2023-10-13 08:29:59 +0530

    Phase 07 - About us page updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -15,8 +15,16 @@ const About = () => {
         NetflixGPT is fully optimized to ensure lightning-fast load times and a smooth user experience, making it perfect for both desktop and mobile devices. Discover a user-friendly interface that offers an immersive viewing experience, all in the palm of your hand.
         <br />
         <br />
-        <a href='f' target='_blank' rel='noreferrer'>Join me</a> in the future of web development, where innovation and user-centric design converge to create NetflixGPT—a showcase of what's possible in the realm of entertainment.
+        Join me in the future of web development, where innovation and user-centric design converge to create NetflixGPT—a showcase of what's possible in the realm of entertainment.
       </p>
+
+      <div className='mb-8 text-gray-200'>
+        Github: <a href='https://github.com/okNeeraj/netflix-gpt' className='hover:text-blue-700 ml-3 text-blue-600 text-sm' target='_blank' rel='noreferrer'>NetflixGPT</a>
+        <br />
+        Live Demo: <a href='https://8ttribute.com' target='_blank' className='hover:text-blue-700 ml-3 text-blue-600 text-sm' rel='noreferrer'>Live Demo</a>
+        <br></br>
+        Linkedin: <a href='https://www.linkedin.com/in/okneerajsingh/' className='hover:text-blue-700 ml-3 text-blue-600 text-sm' target='_blank' rel='noreferrer'>Neeraj Singh</a>
+      </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
         <div className='screen rounded overflow-hidden shadow-xl'>
           <a href={SCREENSHOT.landing} target="_blank" rel="noreferrer">
